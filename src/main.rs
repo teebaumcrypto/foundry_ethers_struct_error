ae2982f51463f62c7c6205dd2c2906844654909d
teebaumCrypto <teebaumcrypto@protonmail.com>
2023-04-09 14:07:04 +0200

    fix build with 2.0.2 ethers & foundry latest

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,22 @@ use anvil_core::eth::{
     transaction::{LegacyTransaction, TransactionKind, TypedTransaction},
 };
 
+use ethers_core::types::{Bytes, Signature};
 use primitive_types::{H160, U256};
 use std::str::FromStr;
 
 fn main() {
-    let tx = TypedTransaction::Legacy(LegacyTransaction {
+    let _tx = TypedTransaction::Legacy(LegacyTransaction {
         nonce: U256::zero(),
         gas_price: U256::zero(),
         gas_limit: U256::zero(),
         kind: TransactionKind::Call(H160::from_str("0xe592427a0aece92de3edee1f18e0157c05861564").unwrap()),
         value: U256::zero(),
-        input: ethers_core::types::bytes::Bytes::default(),
-        signature: ethers_core::types::signature::Signature {
+        input: Bytes::default(),
+        signature: Signature {
             r: U256::zero(),
             s: U256::zero(),
             v: 0u64,
-
         }
     });    
 }
